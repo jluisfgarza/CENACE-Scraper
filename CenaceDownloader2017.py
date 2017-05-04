8e2443e8bf81f0a1fc7ab8e91520a66ab64c697d
jluisfgarza <jluisfgarza@gmail.com>
2017-05-04 17:22:13 -0500

    downloader updates 1/2 done

--- CenaceDownloader2017.py
+++ CenaceDownloader2017.py
@@ -48,6 +48,18 @@ Notes:
                         - PreciosNodosDistrib%20BCS%20MTR_Expost%20Dia%202017-04-18%20v2017%2004%2024_14%2054%2036.csv
 """
 
+# Global Variables
+#Today as DD-MM-YYYY
+"""
+d = "" + datetime.datetime.today().strftime("%d/%m/%Y")
+d = d.replace("/", "-")
+"""
+#TESTING uncoment and comment top 2 lines
+d = "02-05-2017"
+MTR_SIN_d2 = ""
+MTR_BCA_d2 = ""
+MTR_BCS_d2 = ""
+
 def download_file(url, output_file, compressed=True):
     """
     compressed: enable response compression support
@@ -70,13 +82,10 @@ def download_file(url, output_file, compressed=True):
 def getPML_MDA():
     # Save Path for system running the script
     save_path = "C:/Users/e-jlfloresg/Desktop/Python-Requests-CENACE/Cenace_Precios_Energia/Precios Marginales Locales/MDA/"
-    #Today as DD-MM-YYYY
-    d = "" + datetime.datetime.today().strftime("%d/%m/%Y")
-    d = d.replace("/", "-")
-    #TESTING uncoment and comment top 2 lines d = "02-05-2017"
-
-    # SIN
+    
+    ########################################## SIN #########################################
     destino_pml_SIN = os.path.join(save_path, "PRUEBA" + "_SIN_PreciosMargLocalesMDA.csv")
+    # [15] [09] myrange
     for hour in myrange:
         for minute in myrange:
             for second in myrange:
@@ -89,21 +98,24 @@ def getPML_MDA():
                     m = "0"+m
                 if second < 10:
                     s = "0"+s
-                d2 = h + "-" + m + "-" + s
-                # TESTING LOG
-                print(d2)
-                url_pml_SIN = "http://www.cenace.gob.mx/DocsMEM/OpeMdo/PreEner/MargLoc/MDA/Dia/5_" + d + "_" + d2 + "_SIN_PreciosMargLocalesMDA.csv"
+                global MDA_SIN_d2
+                MDA_SIN_d2 = h + "-" + m + "-" + s
+                # TESTING LOG print(MDA_SIN_d2)
+                # TESTING LOG print(d)
+                url_pml_SIN = "http://www.cenace.gob.mx/DocsMEM/OpeMdo/PreEner/MargLoc/MDA/Dia/5_" + d + "_" + MDA_SIN_d2 + "_SIN_PreciosMargLocalesMDA.csv"
                 # TESTING LOG print(url_pml_SIN)
                 a = requests.get(url_pml_SIN)
                 if a.status_code == 404:
                     print ("ERROR 404")
                 else:
                     print("Success")
+                    # TESTING LOG print(url_pml_SIN)
                     pml_file = download_file(url_pml_SIN, destino_pml_SIN)
                     return True
 
-    # BCA
+    ########################################## BCA #########################################
     destino_pml_BCA = os.path.join(save_path, "PRUEBA" + "_BCA_PreciosMargLocalesMDA.csv")
+    # [12] [11] myrange
     for hour in myrange:
         for minute in myrange:
             for second in myrange:
@@ -116,19 +128,24 @@ def getPML_MDA():
                     m = "0"+m
                 if second < 10:
                     s = "0"+s
-                d2 = h + "-" + m + "-" + s
-                # TESTING LOG print(d2)
-                url_pml_BCA = "http://www.cenace.gob.mx/DocsMEM/OpeMdo/PreEner/MargLoc/MDA/Dia/5_" + d + "_" + d2 + "_BCA_PreciosMargLocalesMDA.csv"
+                global MDA_BCA_d2
+                MDA_BCA_d2 = h + "-" + m + "-" + s
+                # TESTING LOG  print(MDA_BCA_d2)
+                # TESTING LOG print(d)
+                url_pml_BCA = "http://www.cenace.gob.mx/DocsMEM/OpeMdo/PreEner/MargLoc/MDA/Dia/5_" + d + "_" + MDA_BCA_d2 + "_BCA_PreciosMargLocalesMDA.csv"
                 # TESTING LOG print(url_pml_BCA)
                 a = requests.get(url_pml_BCA)
                 if a.status_code == 404:
                     print ("ERROR 404")
                 else:
                     print("Success")
+                    # TESTING LOG print(url_pml_BCA)
                     pml_file = download_file(url_pml_BCA, destino_pml_BCA)
                     return True
-    # BCS
+    
+    ########################################## BCS #########################################
     destino_pml_BCS = os.path.join(save_path, "PRUEBA" + "_BCS_PreciosMargLocalesMDA.csv")
+    # [14] [4] myrange
     for hour in myrange:
         for minute in myrange:
             for second in myrange:
@@ -141,112 +158,145 @@ def getPML_MDA():
                     m = "0"+m
                 if second < 10:
                     s = "0"+s
-                d2 = h + "-" + m + "-" + s
-                # TESTING LOG print(d2)
-                url_pml_BCS = "http://www.cenace.gob.mx/DocsMEM/OpeMdo/PreEner/MargLoc/MDA/Dia/5_" + d + "_" + d2 + "_BCS_PreciosMargLocalesMDA.csv"
+                global MDA_BCS_d2
+                MDA_BCS_d2 = h + "-" + m + "-" + s
+                # TESTING LOG  print(MDA_BCS_d2)
+                # TESTING LOG print(d)
+                url_pml_BCS = "http://www.cenace.gob.mx/DocsMEM/OpeMdo/PreEner/MargLoc/MDA/Dia/5_" + d + "_" + MDA_BCS_d2 + "_BCS_PreciosMargLocalesMDA.csv"
                 # TESTING LOG print(url_pml_BCS)
                 a = requests.get(url_pml_BCS)
                 if a.status_code == 404:
                     print ("ERROR 404")
                 else:
                     print("Success")
+                    # TESTING LOG print(url_pml_BCS)
                     pml_file = download_file(url_pml_BCS, destino_pml_BCS)
                     return True
+
     return
 
 def getPND_MDA():
     # Save Path for system running the script
     save_path = "C:/Users/e-jlfloresg/Desktop/Python-Requests-CENACE/Cenace_Precios_Energia/Precios de Nodos Distribuidos/MDA"
-    #Today as DD-MM-YYYY
-    """
-    d = "" + datetime.datetime.today().strftime("%d/%m/%Y")
-    d = d.replace("/", "-")
-    """
-    #TESTING uncoment and comment top 2 lines
-    d = "02-05-2017"
-    # SIN
+
+    ######################################### SIN #########################################
     destino_pnd_SIN = os.path.join(save_path, "PRUEBA" + "_SIN_PreciosNodosDistribuidosMDA.csv")
-    for hour in myrange:
-        for minute in myrange:
-            for second in myrange:
-                h = str(hour)
-                m = str(minute)
-                s = str(second)
-                if hour < 10:
-                    h = "0"+h
-                if minute < 10:
-                    m = "0"+m
-                if second < 10:
-                    s = "0"+s
-                d2 = h + "-" + m + "-" + s
-                # TESTING LOG
-                print(d2)
-                url_pnd_SIN = "http://www.cenace.gob.mx/DocsMEM/OpeMdo/PreEner/MargReg/MDA/Dia/26_" + d + "_" + d2 + "_SIN_PreciosNodosDistribuidosMDA.csv"
-                # TESTING LOG print(url_pnd_SIN)
-                a = requests.get(url_pnd_SIN)
-                if a.status_code == 404:
-                    print ("ERROR 404")
-                else:
-                    print("Success")
-                    pnd_file = download_file(url_pnd_SIN, destino_pnd_SIN)
-                    return True
+    url_pnd_SIN = "http://www.cenace.gob.mx/DocsMEM/OpeMdo/PreEner/MargReg/MDA/Dia/26_" + d + "_" + MDA_SIN_d2 + "_SIN_PreciosNodosDistribuidosMDA.csv"
+    a = requests.get(url_pnd_SIN)
+    if a.status_code == 404:
+        print ("ERROR 404 global var MDA_SIN_d2 not working")
+        # [15] [09] myrange
+        for hour in myrange:
+            for minute in myrange:
+                for second in myrange:
+                    h = str(hour)
+                    m = str(minute)
+                    s = str(second)
+                    if hour < 10:
+                        h = "0"+h
+                    if minute < 10:
+                        m = "0"+m
+                    if second < 10:
+                        s = "0"+s
+                    d2 = h + "-" + m + "-" + s
+                    # TESTING LOG print(d2)
+                    url_pnd_SIN = "http://www.cenace.gob.mx/DocsMEM/OpeMdo/PreEner/MargReg/MDA/Dia/26_" + d + "_" + d2 + "_SIN_PreciosNodosDistribuidosMDA.csv"
+                    # TESTING LOG print(url_pnd_SIN)
+                    a = requests.get(url_pnd_SIN)
+                    if a.status_code == 404:
+                        print ("ERROR 404")
+                    else:
+                        print("Success")
+                        # TESTING LOG print(url_pnd_SIN)
+                        pnd_file = download_file(url_pnd_SIN, destino_pnd_SIN)
+                        return True
+    else:
+        print("Success")
+        # TESTING LOG print(url_pnd_SIN)
+        pnd_file = download_file(url_pnd_SIN, destino_pnd_SIN)
+        return True
 
-    # BCA
+    ########################################## BCA #########################################
     destino_pnd_BCA = os.path.join(save_path, "PRUEBA" + "_BCA_PreciosNodosDistribuidosMDA.csv")
-    for hour in myrange:
-        for minute in myrange:
-            for second in myrange:
-                h = str(hour)
-                m = str(minute)
-                s = str(second)
-                if hour < 10:
-                    h = "0"+h
-                if minute < 10:
-                    m = "0"+m
-                if second < 10:
-                    s = "0"+s
-                d2 = h + "-" + m + "-" + s
-                # TESTING LOG
-                print(d2)
-                url_pnd_BCA = "http://www.cenace.gob.mx/DocsMEM/OpeMdo/PreEner/MargReg/MDA/Dia/26_" + d + "_" + d2 + "_BCA_PreciosNodosDistribuidosMDA.csv"
-                # TESTING LOG print(url_pnd_BCA)
-                a = requests.get(url_pnd_BCA)
-                if a.status_code == 404:
-                    print ("ERROR 404")
-                else:
-                    print("Success")
-                    pnd_file = download_file(url_pnd_BCA, destino_pnd_BCA)
-                    return True
-    # BCS
+    url_pnd_BCA = "http://www.cenace.gob.mx/DocsMEM/OpeMdo/PreEner/MargReg/MDA/Dia/26_" + d + "_" + MDA_BCA_d2 + "_BCA_PreciosNodosDistribuidosMDA.csv"
+    a = requests.get(url_pnd_BCA)
+    if a.status_code == 404:
+        print ("ERROR 404 global var MDA_BCA_d2 not working")
+        # [12] [11] myrange
+        for hour in myrange:
+            for minute in myrange:
+                for second in myrange:
+                    h = str(hour)
+                    m = str(minute)
+                    s = str(second)
+                    if hour < 10:
+                        h = "0"+h
+                    if minute < 10:
+                        m = "0"+m
+                    if second < 10:
+                        s = "0"+s
+                    d2 = h + "-" + m + "-" + s
+                    # TESTING LOG
+                    print(d2)
+                    url_pnd_BCA = "http://www.cenace.gob.mx/DocsMEM/OpeMdo/PreEner/MargReg/MDA/Dia/26_" + d + "_" + d2 + "_BCA_PreciosNodosDistribuidosMDA.csv"
+                    # TESTING LOG print(url_pnd_BCA)
+                    a = requests.get(url_pnd_BCA)
+                    if a.status_code == 404:
+                        print ("ERROR 404")
+                    else:
+                        print("Success")
+                        # TESTING LOG print(url_pnd_BCA)
+                        pnd_file = download_file(url_pnd_BCA, destino_pnd_BCA)
+                        return True
+    else:
+        print("Success")
+        # TESTING LOG print(url_pnd_BCA)
+        pnd_file = download_file(url_pnd_BCA, destino_pnd_BCA)
+        return True
+    
+    ########################################## BCS #########################################
     destino_pnd_BCS = os.path.join(save_path, "PRUEBA" + "_BCS_PreciosNodosDistribuidosMDA.csv")
-    for hour in myrange:
-        for minute in myrange:
-            for second in myrange:
-                h = str(hour)
-                m = str(minute)
-                s = str(second)
-                if hour < 10:
-                    h = "0"+h
-                if minute < 10:
-                    m = "0"+m
-                if second < 10:
-                    s = "0"+s
-                d2 = h + "-" + m + "-" + s
-                # TESTING LOG
-                print(d2)
-                url_pnd_BCS = "http://www.cenace.gob.mx/DocsMEM/OpeMdo/PreEner/MargReg/MDA/Dia/26_" + d + "_" + d2 + "_BCS_PreciosNodosDistribuidosMDA.csv"
-                # TESTING LOG print(url_pnd_BCS)
-                a = requests.get(url_pnd_BCS)
-                if a.status_code == 404:
-                    print ("ERROR 404")
-                else:
-                    print("Success")
-                    pnd_file = download_file(url_pnd_BCS, destino_pnd_BCS)
-                    return True
+    url_pnd_BCS = "http://www.cenace.gob.mx/DocsMEM/OpeMdo/PreEner/MargReg/MDA/Dia/26_" + d + "_" + MDA_BCS_d2 + "_BCS_PreciosNodosDistribuidosMDA.csv"
+    a = requests.get(url_pnd_BCS)
+    if a.status_code == 404:
+        print ("ERROR 404 global var MDA_BCS_d2 not working")
+        # [14] [4] myrange
+        for hour in myrange:
+            for minute in myrange:
+                for second in myrange:
+                    h = str(hour)
+                    m = str(minute)
+                    s = str(second)
+                    if hour < 10:
+                        h = "0"+h
+                    if minute < 10:
+                        m = "0"+m
+                    if second < 10:
+                        s = "0"+s
+                    d2 = h + "-" + m + "-" + s
+                    # TESTING LOG
+                    print(d2)
+                    url_pnd_BCS = "http://www.cenace.gob.mx/DocsMEM/OpeMdo/PreEner/MargReg/MDA/Dia/26_" + d + "_" + d2 + "_BCS_PreciosNodosDistribuidosMDA.csv"
+                    # TESTING LOG print(url_pnd_BCS)
+                    a = requests.get(url_pnd_BCS)
+                    if a.status_code == 404:
+                        print ("ERROR 404")
+                    else:
+                        print("Success")
+                        # TESTING LOG print(url_pnd_BCS)
+                        pnd_file = download_file(url_pnd_BCS, destino_pnd_BCS)
+                        return True
+    else:
+        print("Success")
+        # TESTING LOG print(url_pnd_BCS)
+        pnd_file = download_file(url_pnd_BCS, destino_pnd_BCS)
+        return True
+        
+    return
 
-def getPML_MTR():
+ #def getPML_MTR():
     #http://www.cenace.gob.mx/DocsMEM/OpeMdo/PreEner/MargLoc/MTR/Dia/PreciosMargLocales SIN MTR_Expost Dia 2017-04-26 v2017 05 02_15 36 14.csv
-    return False
+#    return False
 
 #def getPND_MTR():
 #    return False
@@ -254,7 +304,7 @@ def getPML_MTR():
 #   Main Program
 myrange = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59]
 
-#getPML_MDA()
+getPML_MDA()
 getPND_MDA()
 #getPML_MTR()
 #getPND_MTR()
