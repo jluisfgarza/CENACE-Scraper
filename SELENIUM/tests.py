f1dd2263b8a32653dc4e90ff76c588b5e7d1bbc7
jluisfgarza <jluisfgarza@gmail.com>
2017-05-15 12:57:09 -0500

    updates

--- SELENIUM/tests.py
+++ SELENIUM/tests.py
@@ -1,17 +1,53 @@
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+import time
 
+# Google Chorme
 path = "C:/Users/e-jlfloresg/SELENIUM/chromedriver_win32/chromedriver.exe"
 browser = webdriver.Chrome(path)  # Optional argument, if not specified will search path.
-browser.get('http://www.cenace.gob.mx/SIM/VISTA/REPORTES/PreEnergiaSisMEM.aspx')
 
-for i in range(50):
+# Internet Explorer                         
+#path = "C:/Users/e-jlfloresg/SELENIUM/IEDriverServer.exe"
+#browser = webdriver.Ie(path)  # Optional argument, if not specified will search path.
+
+def checkboxes():
+    #PML/MDA/D
+    searchcheckbox("4")
+    #PML/MTR/D
+    searchcheckbox("3")
+    #PND/MDA/D
+    searchcheckbox("5")
+    #PND/MTR/D
+    searchcheckbox("5")
+    return
+                       
+def checktable():    
     try:
-        browser.find.element_by_xpath(".//*[contains(text(),'Precios de la energía')]").click()
-        break
+        browser.find_element_by_xpath("//SPAN[@class='rtMinus rtMinusHover']")         
+        return True              
+    except NoSuchElementException:        
+        print("Error Tabla")                        
+        return False
+
+def searchcheckbox(strindex): 
+    try:
+        browser.find_element_by_xpath("(//SPAN[@class='rtUnchecked'])[" + strindex + "]").click()
+        return
     except NoSuchElementException as e:
-        print("rety")
-        time.sleep(1)
-else:
-    print ("test failed")
+        print("Error finding the element")
+        time.sleep(1)        
+    return      
+
+### Main Program ###
+browser.get('http://www.cenace.gob.mx/SIM/VISTA/REPORTES/PreEnergiaSisMEM.aspx')
+time.sleep(3)
+
+for i in range(1, 6):    
+    if (checktable() == False):
+        browser.refresh()
+        checktable()
+    else:
+        checkboxes()
+
+        
+    
