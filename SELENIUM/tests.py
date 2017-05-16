dfe38fceacfdca03309f388ef61d18cea3a168ad
jluisfgarza <jluisfgarza@gmail.com>
2017-05-16 09:57:49 -0500

    load and checkboxes completed

--- SELENIUM/tests.py
+++ SELENIUM/tests.py
@@ -1,53 +1,53 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
-import time
+from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import ElementNotVisibleException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+def loadsite() :
+    try:
+        element_present = EC.presence_of_element_located((By.ID, 'ctl00_ContentPlaceHolder1_treePrincipal'))
+        WebDriverWait(browser, delay).until(element_present)        
+        print ("Page is ready!")
+        checkboxes()
+        return
+    except TimeoutException:
+        print ("Error Code 1: Error Cargando página")
+    except ElementNotVisibleException as nv:
+        print("Error Code 2: Error cargado la tabla")    
+    browser.refresh()
+    loadsite()
+    return
+
+def checkboxes():
+    #PML/MDA/D    
+    searchcheckbox('4')
+    #PML/MTR/D
+    searchcheckbox('3')
+    #PND/MDA/D
+    searchcheckbox('5')
+    #PND/MTR/D
+    searchcheckbox('5')
+    return
+                       
+def searchcheckbox(mychar): 
+    try:
+        browser.find_element_by_xpath("(//SPAN[@class='rtUnchecked'])[" + mychar + "]").click() 
+        return
+    except NoSuchElementException as e:
+        print("Error finding the element")
+    return      
+
+### Main Program ###
 
 # Google Chorme
 path = "C:/Users/e-jlfloresg/SELENIUM/chromedriver_win32/chromedriver.exe"
 browser = webdriver.Chrome(path)  # Optional argument, if not specified will search path.
-
-# Internet Explorer                         
-#path = "C:/Users/e-jlfloresg/SELENIUM/IEDriverServer.exe"
-#browser = webdriver.Ie(path)  # Optional argument, if not specified will search path.
-
-def checkboxes():
-    #PML/MDA/D
-    searchcheckbox("4")
-    #PML/MTR/D
-    searchcheckbox("3")
-    #PND/MDA/D
-    #searchcheckbox("5")
-    #PND/MTR/D
-    #searchcheckbox("5")
-    return
-                       
-def checktable():    
-    try:
-        browser.find_element_by_xpath("(//SPAN[@class='rtUnchecked'])[1]")         
-        return True              
-    except NoSuchElementException:        
-        print("Error Tabla")                        
-        return False
-
-def searchcheckbox(strindex): 
-    try:
-        browser.find_element_by_xpath("(//SPAN[@class='rtUnchecked'])[" + strindex + "]").click() 
-        return
-    except NoSuchElementException as e:
-        print("Error finding the element")
-        time.sleep(1)        
-    return      
-
-### Main Program ###
 browser.get('http://www.cenace.gob.mx/SIM/VISTA/REPORTES/PreEnergiaSisMEM.aspx')
-time.sleep(3)
-
-for i in range(1,6):
-    if (checktable() == False):        
-        browser.refresh()        
-        time.sleep(10)
-    else:
-        checkboxes()
+delay = 15 #seconds
+loadsite()
 
         
     
