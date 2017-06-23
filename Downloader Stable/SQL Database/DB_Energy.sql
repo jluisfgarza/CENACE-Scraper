fb537fed9389afd31523fa13f8718d8df836f411
jluisfgarza <jluisfgarza@gmail.com>
2017-06-23 09:15:09 -0500

    sql query update

--- Downloader Stable/SQL Database/DB_Energy.sql
+++ Downloader Stable/SQL Database/DB_Energy.sql
@@ -7,26 +7,27 @@ USE PreciosEnergía
 
 -- 1. Create Tables
 CREATE TABLE PML (
-  Hora int NOT NULL,
-  Nodo varchar(20) NOT NULL,
-  Precio decimal(10,2) NOT NULL,
-  Energía decimal(10,2) NOT NULL,
-  Pérdidas decimal(10,2) NOT NULL,
-  Congestión varchar(10) NOT NULL,
-  timestamp varchar(30) NOT NULL,
-  Zona int NOT NULL,
-  Subestación varchar(10) NOT NULL,
-  tensión int NOT NULL,
+  [Hora] int NOT NULL,
+  [Nodo] varchar(20) NOT NULL,
+  [Precio] decimal(10,2) NOT NULL,
+  [Energía] decimal(10,2) NOT NULL,
+  [Pérdidas] decimal(10,2) NOT NULL,
+  [Congestión] varchar(10) NOT NULL,
+  [timestamp] varchar(30) NOT NULL,
+  [Tipo] varchar(3) NOT NULL,
+  [Sistema] varchar(3) NOT NULL
 );
 
 CREATE TABLE PND (
-  Hora int NOT NULL,
+  [Hora] int NOT NULL,
   [Zona de Carga] varchar(30) NOT NULL,
   [Precio Zonal] decimal(10,2) NOT NULL,
-  Energía decimal(10,2) NOT NULL,
-  Pérdidas decimal(10,2) NOT NULL,
-  Congestión decimal(10,2) NOT NULL,
-  timestamp varchar(30) NOT NULL,
+  [Energía] decimal(10,2) NOT NULL,
+  [Pérdidas] decimal(10,2) NOT NULL,
+  [Congestión] decimal(10,2) NOT NULL,
+  [timestamp] varchar(30) NOT NULL,
+  [Tipo] varchar(3) NOT NULL,
+  [Sistema] varchar(3) NOT NULL
 );
 
 -- 2. Python script to import csv files
@@ -47,20 +48,19 @@ EXEC sp_spaceused 'PND'
 -- 4. Space used by tables
   -- PML full info 2016
 CREATE TABLE PMLTEMP(
-  Hora int NOT NULL,
-  Nodo varchar(20) NOT NULL,
-  Precio decimal(10,2) NOT NULL,
-  Energía decimal(10,2) NOT NULL,
-  Pérdidas decimal(10,2) NOT NULL,
-  Congestión varchar(10) NOT NULL,
-  timestamp varchar(30) NOT NULL,
-  Zona int NOT NULL,
-  Subestación varchar(10) NOT NULL,
-  tensión int NOT NULL
+  [Hora] int NOT NULL,
+  [Nodo] varchar(20) NOT NULL,
+  [Precio] decimal(10,2) NOT NULL,
+  [Energía] decimal(10,2) NOT NULL,
+  [Pérdidas] decimal(10,2) NOT NULL,
+  [Congestión] varchar(10) NOT NULL,
+  [timestamp] varchar(30) NOT NULL,
+  [Tipo] varchar(3) NOT NULL,
+  [Sistema] varchar(3) NOT NULL
 );
 
-INSERT INTO PMLTEMP ([Hora], [Nodo], [Precio], [Energía], [Pérdidas], [Congestión], [timestamp], [Zona], [Subestación], [tensión])
-SELECT [Hora], [Nodo], [Precio], [Energía], [Pérdidas], [Congestión], [timestamp], [Zona], [Subestación], [tensión]
+INSERT INTO PMLTEMP ([Hora], [Nodo], [Precio], [Energía], [Pérdidas], [Congestión], [timestamp], [Tipo], [Sistema])
+SELECT [Hora], [Nodo], [Precio], [Energía], [Pérdidas], [Congestión], [timestamp], [Tipo], [Sistema]
 FROM PML
 Where [timestamp] like '%2016%'
 
@@ -69,17 +69,19 @@ DROP TABLE PMLTEMP
 
   -- PND full info 2017
 CREATE TABLE PNDTEMP(
-  Hora int NOT NULL,
+  [Hora] int NOT NULL,
   [Zona de Carga] varchar(30) NOT NULL,
   [Precio Zonal] decimal(10,2) NOT NULL,
-  Energía decimal(10,2) NOT NULL,
-  Pérdidas decimal(10,2) NOT NULL,
-  Congestión decimal(10,2) NOT NULL,
-  timestamp varchar(30) NOT NULL,
+  [Energía] decimal(10,2) NOT NULL,
+  [Pérdidas] decimal(10,2) NOT NULL,
+  [Congestión] decimal(10,2) NOT NULL,
+  [timestamp] varchar(30) NOT NULL,
+  [Tipo] varchar(3) NOT NULL,
+  [Sistema] varchar(3) NOT NULL
 );
 
-INSERT INTO PNDTEMP ([Hora], [Zona de Carga], [Precio Zonal], [Energía], [Pérdidas], [Congestión], [timestamp])
-SELECT [Hora], [Zona de Carga], [Precio Zonal], [Energía], [Pérdidas], [Congestión], [timestamp]
+INSERT INTO PNDTEMP ([Hora], [Zona de Carga], [Precio Zonal], [Energía], [Pérdidas], [Congestión], [timestamp], [Tipo], [Sistema])
+SELECT [Hora], [Zona de Carga], [Precio Zonal], [Energía], [Pérdidas], [Congestión], [timestamp], [Tipo], [Sistema]
 FROM PND
 Where [timestamp] like '%2016%'
 
