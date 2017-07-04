bd4994b5548243ec514ce8f050b9249e391389ff
jluisfgarza <jluisfgarza@gmail.com>
2017-07-04 12:55:28 -0500

    dashboard init

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,16 @@
+/*
+Hora
+Clave del nodo
+Precio marginal local ($/MWh)
+Componente de energia ($/MWh)
+Componente de perdidas ($/MWh)
+Componente de congestion ($/MWh)
+*/
 var data = [];
+var jasondata = [{}];
+
 d3.queue()
-    .defer(d3.csv, "test1.csv")
+    .defer(d3.csv, "PythonTool/PastCSVBackup/PML/MDA/2016/PreciosMargLocales BCA MDA Mes Abr01 v2016 09 01_08 54 06.csv")
     .defer(d3.csv, "test2.csv")
     .await(analyze);
 
@@ -8,57 +18,26 @@ function analyze(error, test1, test2) {
     if (error) {
         console.log(error);
     }
-    console.log(d3.merge([test1, test2]));
-    data = d3.merge([test1, test2]);
-    //readfile(test1);
-    //readfile(test2);
-    //console.log(test1);
-    //console.log(test2);
+
+    data = readfile(d3.merge([test1, test2]));
+    jsondata = (JSON.stringify(data, null, "\t"));
+    //console.log(jsondata);
+    //console.log(data);  
+
 }
 
-function readfile(filename) {
-    d3.csv("test2.csv", function (data) {
-        data.forEach(function (d) {
-            d.Hora = d["Hora"],
-                d.Clave_del_nodo = +d["Clave del nodo"],
-                d.Precio_marginal_local = +d["Precio marginal local ($/MWh)"],
-                d.Componente_de_energia = +d["Componente de energia ($/MWh)"],
-                d.Componente_de_perdidas = +d["Componente de perdidas ($/MWh)"],
-                d.Componente_de_congestion = +d["Componente de congestion ($/MWh)"]
-        });
-        console.log(data);
-    });
-};
-
-// https://bl.ocks.org/mbostock/4062085
-var width = 500,
-    barHeight = 500;
-
-var x = d3.scaleLinear()
-    .domain([0, d3.max(data)])
-    .range([0, width]);
-
-var chart = d3.select(".chart")
-    .attr("width", width)
-    .attr("height", barHeight * data.length);
-
-var bar = chart.selectAll("g")
-    .data(data)
-    .enter().append("g")
-    .attr("transform", function (d, i) {
-        return "translate(0," + i * barHeight + ")";
-    });
-
-bar.append("rect")
-    .attr("width", x)
-    .attr("height", barHeight - 1);
-
-bar.append("text")
-    .attr("x", function (d) {
-        return x(d) - 3;
+function readfile(data) {
+    data.forEach(function (d) {
+        temp = d["Fecha"];
+        temp = parseDate(temp);
+        //console.log(Date.parse(temp));
+        d["Fecha"] = temp,
+        d["Hora"] = +d["Hora"],
+        d["Clave del nodo"] = d["Clave del nodo"],
+        d["Precio marginal local ($/MWh)"] = +d["Precio marginal local ($/MWh)"],
+        d["Componente de energia ($/MWh)"] = +d["Componente de energia ($/MWh)"],
+        d["Componente de perdidas ($/MWh)"] = +d["Componente de perdidas ($/MWh)"],
+        d["Componente de congestion ($/MWh)"] = +d["Componente de congestion ($/MWh)"]
     })
-    .attr("y", barHeight / 2)
-    .attr("dy", ".35em")
-    .text(function (d) {
-        return d;
-    });
+    return (data);
+};
